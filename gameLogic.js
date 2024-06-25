6d094a4a6b7c98b623460af7e9557c3d0df8466f
raghavkhare98 <khare08rma@gmail.com>
2024-06-25 16:38:48 -0400

    Add DOM methods in place of commit messages and create UI

--- gameLogic.js
+++ gameLogic.js
@@ -2,75 +2,95 @@ const choices = ["rock", "paper", "scissor"];
 let roundCount = 0;
 let humanScore = 0;
 let computerScore = 0;
-const roundCountText = document.querySelector('.round-count');
-const humanScoreText = document.querySelector('.human-score');
-const computerScoreText = document.querySelector('.computer-score');
+const roundCountText = document.querySelector('#round-count');
+const humanScoreText = document.querySelector('#human-score');
+const computerScoreText = document.querySelector('#computer-score');
 function getComputerChoice(){
-    const computerChoiceText = document.querySelector('.computer-choice')
+    const computerChoiceText = document.querySelector('#computer-choice')
     let computerChoice = choices[Math.floor(Math.random(0) * 3)];
     computerChoiceText.textContent = `The computer chose ${computerChoice}`;
     return computerChoice;
 }
 
 function getHumanChoice(){
-    const userChoiceText = document.querySelector('.user-choice')
-    let userChoice = " ";
-    while(choices.includes(userChoice.toLocaleLowerCase()) != true){
-        userChoice = prompt("Enter a value from choices provided in the homepage:");
-        if(choices.includes(userChoice.toLocaleLowerCase()) != true){
-            alert("Please select a valid response from the below provided options");
-        }
-    }
-    userChoiceText.textContent = `The user chose ${userChoice}`;
-    return userChoice;
+    const userChoiceText = document.querySelector('#user-choice')
+    let userChoice = "s";
+    const rockButton = document.querySelector('#rock-button');
+    const paperButton = document.querySelector('#paper-button');
+    const scissorButton = document.querySelector('#scissors-button');
+    rockButton.addEventListener("click", () => {
+        userChoice = "rock";
+        userChoiceText.textContent = `The user chose ${userChoice}`;
+        return userChoice;
+    })
+    paperButton.addEventListener("click", () => {
+        userChoice = "paper";
+        userChoiceText.textContent = `The user chose ${userChoice}`;
+        return userChoice;
+    })
+    scissorButton.addEventListener("click", () => {
+        userChoice = "scissor";
+        userChoiceText.textContent = `The user chose ${userChoice}`;
+        return userChoice;
+    })
 }
 
 function playRound(humanSelection, computerSelection){
+    let roundResult = document.querySelector('#round-result');
     switch(humanSelection){
         case "rock":
             if(computerSelection.toLowerCase() === "paper"){
-                console.log("You lose! Paper beats Rock")
+                roundResult.textContent = "You lose! Paper beats Rock";
                 computerScore++;
             } else if(computerSelection.toLowerCase() === "scissor"){
-                console.log("You win! Rock beats Scissor")
+                roundResult.textContent = "You win! Rock beats Scissor"
                 humanScore++;
             } else{
-                console.log("It's a tie.")
+                roundResult.textContent = "It's a tie.";
             }
             break;
         case "paper":
             if(computerSelection.toLowerCase() === "rock"){
-                console.log("You win! Paper beats Rock")
+                roundResult.textContent = "You win! Paper beats Rock";
                 humanScore++;
             } else if(computerSelection.toLowerCase() === "scissor"){
-                console.log("You lose! Scissor beats Paper")
+                roundResult.textContent = "You lose! Scissor beats Paper";
                 computerScore++;
             } else{
-                console.log("It's a tie.")
+                roundResult.textContent = "It's a tie.";
             }
             break;
         case "scissor":
             if(computerSelection.toLowerCase() === "paper"){
-                console.log("You win! Scissor beats paper")
+                roundResult.textContent = "You win! Scissor beats paper";
                 humanScore++;
             } else if(computerSelection.toLowerCase() === "rock"){
-                console.log("You lose! Rock beats Scissor")
+                roundResult.textContent = "You lose! Rock beats Scissor";
                 computerScore++;
             } else{
-                console.log("It's a tie.")
+                roundResult.textContent = "It's a tie.";
             }
             break;    
     }
     humanScoreText.textContent = `Your score: ${humanScore}`;
     computerScoreText.textContent = `Computer's score: ${computerScore}`;
-
 }
 
 function playGame(){
-    while(roundCount <= 4){
-        playRound(getHumanChoice(), getComputerChoice())
-        roundCount++;
-    }
+    let gameResult = document.querySelector('#game-result');
+    // while(humanScore < 5 || computerScore < 5){ 
+    //     playRound(getHumanChoice(), getComputerChoice())
+    //     roundCount++;
+    // }
+    playRound(getHumanChoice(), getComputerChoice())
+    roundCount++;
     roundCountText.textContent = `Number of round played: ${roundCount}`;
+    if(humanScore == 5){
+        gameResult.textContent = `Congratulations! You won!`
+    } else if(computerScore == 5){
+        gameResult.textContent = `Nice try! The computer won this set`;
+    }
 }
+console.log(humanScore);
 playGame();
+
